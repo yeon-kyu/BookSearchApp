7df14a373ed5c6529106e6ff1174f131f0d90560
yeon-kyu <45001093+yeon-kyu@users.noreply.github.com>
2022-02-17 04:25:10 +0900

    Add Test for DetailRepository

---
+++ app/src/test/java/com/yeonkyu/booksearchapp/repository/DetailRepositoryTest.kt
@@ -0,0 +1,51 @@
+package com.yeonkyu.booksearchapp.repository
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.yeonkyu.booksearchapp.data.mapper.toBookInfo
+import com.yeonkyu.booksearchapp.data.model.BookInfo
+import com.yeonkyu.booksearchapp.util.MockUtil.mockBookInfoResponse
+import com.yeonkyu.itbooksdk.ItBookStore
+import com.yeonkyu.itbooksdk.api.ItBookInfoHandler
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito
+import org.mockito.Mockito.times
+import org.mockito.kotlin.any
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+
+class DetailRepositoryTest {
+
+    private lateinit var repository: DetailRepository
+    private lateinit var itBookStore: ItBookStore
+
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setup() {
+        itBookStore = mock()
+        repository = DetailRepositoryImpl(itBookStore)
+    }
+
+    @Test
+    fun `get Book Info Test`() {
+        // given
+        val mockData = mockBookInfoResponse()
+        Mockito.`when`(itBookStore.fetchBookInfo(any(), any())).thenAnswer {
+            (it.arguments[1] as ItBookInfoHandler).onSuccess(mockData)
+        }
+
+        // when
+        repository.getBookInfo(
+            isbn = "123123",
+            onSuccess = {
+                assertEquals(mockData.toBookInfo(), it)
+            },
+            onFail = { }
+        )
+        verify(itBookStore, times(1)).fetchBookInfo(any(), any())
+    }
+}
--- app/src/test/java/com/yeonkyu/booksearchapp/util/MockUtil.kt
+++ app/src/test/java/com/yeonkyu/booksearchapp/util/MockUtil.kt
@@ -1,5 +1,6 @@
 package com.yeonkyu.booksearchapp.util
 
+import com.yeonkyu.itbooksdk.response.BookInfoResponse
 import com.yeonkyu.itbooksdk.response.SearchListResponse
 import com.yeonkyu.itbooksdk.response.SearchResponse
 import org.mockito.Mockito
@@ -22,5 +23,16 @@ object MockUtil {
         bookList = listOf(
             mockSearchResponse_Java()
         )
+    )
+
+    fun mockBookInfoResponse() = BookInfoResponse(
+        error = "0",
+        title = "Practical MongoDB",
+        subtitle = "Architecting, Developing, and Administering MongoDB",
+        authors = "Shakuntala Gupta Edward, Navin Sabharwal",
+        publisher = "Apress",
+        year = "2015",
+        desc = "Practical Guide to MongoDB: Architecting, Developing, and Administering MongoDB begins with a short introduction to the basics of NoSQL databases and then introduces readers to MongoDB - the leading document based NoSQL database, acquainting them step-by-step with all aspects of MongoDB.Practical Gu...",
+        img = "https://itbook.store/img/books/9781484206485.png"
     )
 }
