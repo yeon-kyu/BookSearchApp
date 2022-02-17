297ba007f91c9ffbf6ce68196f6ebb8a92fd0d50
yeon-kyu <45001093+yeon-kyu@users.noreply.github.com>
2022-02-17 18:20:50 +0900

    Remove Startup dependency in App build.gradle

--- README.md
+++ README.md
@@ -11,7 +11,6 @@
 - Lifecycle(LiveData)
 - DataBinding
 - ViewModel
-- Startup
 
 ### Unit Test
 - JUnit
--- app/src/main/java/com/yeonkyu/booksearchapp/BookSearchApplication.kt
+++ app/src/main/java/com/yeonkyu/booksearchapp/BookSearchApplication.kt
@@ -1,7 +1,19 @@
 package com.yeonkyu.booksearchapp
 
 import android.app.Application
+import com.yeonkyu.itbooksdk.ItBookStore
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class BookSearchApplication : Application()
+class BookSearchApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        ItBookStore.initItBook()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
